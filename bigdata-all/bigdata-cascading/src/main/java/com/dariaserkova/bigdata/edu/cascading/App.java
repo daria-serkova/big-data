415a4c04e1f192e1996bd689bbe2d5ce8f71b387
Daria_Serkova <serkova-darya@mail.ru>
2014-07-18 17:50:48 +0400

    #1 Addad stubs for classes

---
+++ bigdata-all/bigdata-cascading/src/main/java/com/dariaserkova/bigdata/edu/cascading/App.java
@@ -0,0 +1,13 @@
+package com.dariaserkova.bigdata.edu.cascading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
